8e8e7c4407d705c6c1dd8724ed41ea9c4af33f83
Pierre-Gilles Levallois <pierregille.levallois@gmail.com>
2019-07-23 12:09:23 +0200

    migration to vscode/platformio IDE

--- include/config.h
+++ include/config.h
@@ -3,7 +3,7 @@
 //--------------------------------------------------------------------------------
 // AP mode name and password
 const char* WiFiAPName = "LightControlAP";
-const char* WiFiAPPSK = "mySecretPassword";
+const char* WiFiAPPSK = "ComptoirStCyr2019";
 
 // IP configuration for AP
 IPAddress WiFiIp(192, 168, 10, 1);
@@ -25,7 +25,43 @@ const bool apMode = true; // Whether if ESP8266 should connect to an existing WI
 #define NUM_STRIPS    4
 #define MAX_LEDS_PER_STRIP 300
 // please add one number of leds per strip needed
-const int num_leds_list[4] =  { 85, 98, 168, 300 };
+//const int num_leds_list[4] =  { 85, 98, 168, 300 };
+const int num_leds_list[NUM_STRIPS] =  { 8, 8, 8, 8 };
+
+// specific correction for each strip, if needed
+/* Color corrections from FastLED : 
+    TypicalSMD5050 =0xFFB0F0,
+    TypicalLEDStrip =0xFFB0F0,
+    Typical8mmPixel =0xFFE08C,
+    TypicalPixelString =0xFFE08C,
+    UncorrectedColor =0xFFFFFF 
+ */
+const CRGB light_correction_list[NUM_STRIPS] = { TypicalLEDStrip, TypicalLEDStrip, TypicalLEDStrip, TypicalLEDStrip };
+
+ // Specific temperature corrections from FastLED : 
+ /*
+Candle =0xFF9329,
+Tungsten40W =0xFFC58F,
+Tungsten100W =0xFFD6AA,
+Halogen =0xFFF1E0,
+CarbonArc =0xFFFAF4,
+HighNoonSun =0xFFFFFB,
+DirectSunlight =0xFFFFFF,
+OvercastSky =0xC9E2FF,
+ClearBlueSky =0x409CFF,
+WarmFluorescent =0xFFF4E5,
+StandardFluorescent =0xF4FFFA,
+CoolWhiteFluorescent =0xD4EBFF,
+FullSpectrumFluorescent =0xFFF4F2,
+GrowLightFluorescent =0xFFEFF7,
+BlackLightFluorescent =0xA700FF,
+MercuryVapor =0xD8F7FF,
+SodiumVapor =0xFFD1B2,
+MetalHalide =0xF2FCFF,
+HighPressureSodium =0xFFB74C,
+UncorrectedTemperature =0xFFFFFF
+ */
+const CRGB temp_correction_list[NUM_STRIPS] = { UncorrectedTemperature, UncorrectedTemperature, ClearBlueSky, UncorrectedTemperature };
 
 #define MILLI_AMPS         25000 // IMPORTANT: set the max milli-Amps of your power supply (4A = 4000mA)
 #define FRAMES_PER_SECOND  120  // here you can control the speed. With the Access Point / Web Server the animations run a bit slower.
